bf493d2a3df673951a8317b77873eb0b9fa226af
Wahib Kapdi <kanchuking1@gmail.com>
2023-10-29 18:04:21 +0530

    Portfolio Page Added

--- js/initialize.js
+++ js/initialize.js
@@ -42,6 +42,23 @@ const initialize = async () => {
     for (const workex of workexps) {
         appendInnerHTMLById(generateWorkexHTML(workex), "workex-list");
     }
+
+    window.onscroll = function() {myFunction()};
+
+    // Get the navbar
+    var navbar = document.getElementById("nav-header");
+
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+    if (window.scrollY >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+    }
 }
 
 try {
